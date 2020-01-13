5deeb6c2eb7cb7074e6d654649d59095d008416c
Daniel Florez <danielflorez111@hotmail.com>
2020-01-12 23:27:52 -0500

    Anagram

---
+++ 02-arrays-vs-objects/objects.js
@@ -0,0 +1,10 @@
+var instructor = {
+    firstName: "Kelly",
+    isInstructor: true,
+    favoriteNumbers: [1, 2, 3],
+};
+
+console.log(Object.keys(instructor));
+console.log(Object.values(instructor));
+console.log(Object.entries(instructor));
+console.log(instructor.hasOwnProperty("firstName"));
