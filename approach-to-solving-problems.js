fa2f3f4ec9ff31511a9a32f00295a5a261fcc68a
Daniel Florez <danielflorez111@gmail.com>
2021-02-05 16:44:22 -0500

    Approach to solving problems

---
+++ approach-to-solving-problems.js
@@ -0,0 +1,39 @@
+// How do you improve?
+// 1. Devise a plan for solving problems
+// 2. Master common problem solving patterns
+
+// Devise a plan for solving problems:
+//  - Understand the problem
+//  - Explore concrete examples
+//  - Break it down
+//  - Solve/Simplify
+//  - Look back and refactor
+
+// Understand the problem
+//  - Can I restate the problem in my own words?
+//  - What are the inputs?
+//  - What are the outputs?
+//  - Can the outputs be determined from the inputs?
+//  - Do I have enough information to solve the problem?
+//  - How should I label the important pieces of data that are a part of the problem?
+
+// Explore concrete examples
+//  - Simple examples
+//  - More complex examples
+//  - Empty inputs
+//  - Invalid inputs
+
+// Break it down
+//  - Explicitly write out the steps you need to take
+
+// Solve/Simplify
+//  - Find the core difficulty in what you're trying to do
+//  - Temporarily ignore that difficulty
+//  - Write a simplified solution
+//  - Then incorporate that difficulty back in
+
+// Look back and refactor
+//  - Can you check the result?
+//  - Can you improve the performance of your solution?
+//  - Can you think of other ways to refactor?
+//  - How have other people solved this problem?
